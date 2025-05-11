f895e63e2157e0ee5f6b9cae14e1fef4493392ab
isamorro <isabelmorro@hotmail.com>
2025-05-11 18:57:20 +0200

    productos d base de datos metidos

--- tienda-virtual/frontend/tienda-frontend/src/App.jsx
+++ tienda-virtual/frontend/tienda-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Home from './pages/Home';
 
 function App() {
+
   const [carrito, setCarrito] = useState([]);
 
   const handleAddToCart = producto => {
@@ -9,18 +10,27 @@ function App() {
   };
 
   return (
+    
     <div>
+    
       <h1 style={{ textAlign: 'center' }}>Tienda Virtual</h1>
+    
       <Home onAddToCart={handleAddToCart} />
+    
       <div style={{ position: 'fixed', top: 20, right: 20, background: '#eee', padding: '1rem' }}>
+     
         <h2>🛒 Carrito ({carrito.length})</h2>
+     
         <ul>
           {carrito.map((item, index) => (
-            <li key={index}>{item.nombre} - {item.precio} €</li>
+            <li key={index}> {item.nombre} - {item.precio} € - {item.categoria} - {producto.peso_o_resistencia} </li>
           ))}
+     
         </ul>
-        <p><b>Total:</b> {carrito.reduce((acc, item) => acc + parseFloat(item.precio), 0).toFixed(2)} €</p>
+     
+        <p> <b>Total:</b> {carrito.reduce((acc, item) => acc + parseFloat(item.precio), 0).toFixed(2)} €</p>
       </div>
+    
     </div>
   );
 }
--- tienda-virtual/frontend/tienda-frontend/src/components/ProductCard.jsx
+++ tienda-virtual/frontend/tienda-frontend/src/components/ProductCard.jsx
@@ -1,16 +1,19 @@
 function ProductCard({ producto, onAddToCart }) {
-    
-    return (
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '1rem', width: '200px' }}>
+      <img
+        src={producto.imagen_url}
+        alt={producto.nombre}
+        style={{ width: '100%', height: '150px', objectFit: 'cover' }}
+      />
+      <h3> {producto.nombre} </h3>
+      <p> {producto.precio} € </p>
+      <p> {producto.peso_o_resistencia} </p>
+      <p> {producto.categoria} </p>
+      
+      <button onClick={() => onAddToCart(producto)}>Añadir al carrito</button>
+    </div>
+  );
+}
 
-      <div style={{ border: '1px solid #ccc', padding: '1rem', width: '200px' }}>
-        <img src={producto.imagen} alt={producto.nombre} style={{ width: '100%' }} />
-        <h3>{producto.nombre}</h3>
-        <p>{producto.precio} €</p>
-        <button onClick={() => onAddToCart(producto)}>Añadir al carrito</button>
-      </div>
-    
-    );
-  }
-  
-  export default ProductCard;
-  
+export default ProductCard;
